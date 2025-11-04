9ec89975cc7c976929352c1733cda47f90bc488a
Segoul01 <129515380+Segoul01@users.noreply.github.com>
2025-11-04 19:55:46 +0000

    no fkin idea

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,50 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
+import Product from './model/product.model.js';
+
 const app = express();
 
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.get("/", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.write(JSON.stringify({message: "This works!!"}));
-    res.end();
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.log("Error in fetching products: ", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
 })
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body; // user will send this data
+
+    if (!product.name || !product.price || !product.image){
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.error(`Error in create product: ${error.message}`);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
+
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product Deleted"});
+    } catch (error) {
+        res.status(404).json({ success: false, message: "Product Not Found" });
+    }
+});
 
 
 app.listen(8000, () => {
