a0fa22245a8cc0ff862343bdc6afa340db3c9171
Segoul01 <129515380+Segoul01@users.noreply.github.com>
2025-11-05 23:11:52 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ import { connectDB } from './config/db.js';
 import productRoutes from "./routes/product.route.js";
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server started at port: ${process.env.PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server started at port: ${PORT}`);
     connectDB();
 });
